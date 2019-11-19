8106a7661602285ba4876859b52f3864d70484b2
Will Frey <will.frey@Wills-MacBook-Pro.local>
2019-11-19 18:19:46 -0500

    Update workouts.py

--- 109/workouts.py
+++ 109/workouts.py
@@ -1,4 +1,4 @@
-WORKOUT_SCHEDULE = {
+workout_schedule = {
     "Friday": "Shoulders",
     "Monday": "Chest+biceps",
     "Saturday": "Rest",
@@ -7,33 +7,30 @@ WORKOUT_SCHEDULE = {
     "Tuesday": "Back+triceps",
     "Wednesday": "Core",
 }
-REST, CHILL, GO_TRAIN = "Rest", "Chill out!", "Go train {}"
+rest, chill, go_train = "Rest", "Chill out!", "Go train {}"
 
 
-def get_workout_motd(day: str):
-    """
-    First title case the passed in day argument (so monday or MONDAY
-    both result in Monday).
+def get_workout_motd(day):
+    """First title case the passed in day argument
+       (so monday or MONDAY both result in Monday).
 
-    Then check if this title cased day is in the given
-    workout_schedule dict.
+       Then check if this title cased day is in the given workout_schedule dict.
 
-    If it is retrieve the workout (value), if not raise a KeyError.
+       If it is retrieve the workout (value), if not raise a KeyError.
 
-    Return the chill variable if it's a rest day (Saturday / Sunday),
-    else return the go_train variable with the workout interpolated.
+       Return the chill variable if it's a rest day (Saturday / Sunday),
+       else return the go_train variable with the workout interpolated.
 
-    Examples:
-      - if day is Saturday -> return 'Chill out!'
-      - if day is Thursday -> return 'Go train Legs'
-      - if day is Sunday -> return 'Chill out!'
-      - if day is Monday -> return 'Go train Chest+biceps'
+       Examples:
+       - if day is Saturday -> return 'Chill out!'
+       - if day is Thursday -> return 'Go train Legs'
+       - if day is Sunday -> return 'Chill out!'
+       - if day is Monday -> return 'Go train Chest+biceps'
 
-    Trivia: /etc/motd is a file on Unix-like systems that contains a
-    'message of the day'
-    """
-    day = day.title()
-    activity = WORKOUT_SCHEDULE.get(day)
-    if activity is None:
-        raise KeyError(day)
-    return CHILL if activity == REST else GO_TRAIN.format(activity)
+       Trivia: /etc/motd is a file on Unix-like systems that contains
+       a 'message of the day'"""
+    try:
+        workout = workout_schedule[day.title()]
+    except KeyError:
+        raise KeyError("Workout does not exist.")
+    return chill if workout == rest else go_train.format(workout)
